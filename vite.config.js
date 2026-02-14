d6f2f24ceff6581aa535521a864246c5f12bf08b
Millie Ren <renyifei2017@hotmail.com>
2026-02-14 15:56:02 +0800

    change everything back to original setup

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: 'public',
+  //root: 'public',
   plugins: [react()],
-  build: {
+  /*build: {
     outDir: '../dist'
-  }
+  }*/
 })
